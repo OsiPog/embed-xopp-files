acfa778bce717d8e918a1685bf5ae9dbb965129a
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-04-08 12:18:44 +0200

    feat: `getPdfPath` method to create a preview for xopp file

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, FileSystemAdapter, TFile } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, FileSystemAdapter, TFile, FileStats, Stat } from 'obsidian';
+import { exec } from 'child_process';
 
 interface XoppPluginSettings {
-    mySetting: string;
+    importedPath: string;
 }
 
 const DEFAULT_SETTINGS: XoppPluginSettings = {
-    mySetting: 'default'
+    importedPath: 'xopp',
 }
 
 export default class XoppPlugin extends Plugin {
@@ -37,6 +38,31 @@ export default class XoppPlugin extends Plugin {
 
     }
 
+    /**
+     * Returns the generated preview pdf file for the given xopp file. If the pdf file is too old/doesn't exist generate one
+     * A pdf filename will have the form: path_to_xopp_xoppfilename.xopp.pdf
+     * @param xoppFile
+     */
+    async getPdfPath(xoppFile: TFile): Promise<string> {
+        const pdfPath = `${this.settings.importedPath}/preview/${xoppFile.path.replace(/\/|\\/, '_')}.pdf`;
+
+        if (await this.app.vault.adapter.exists(pdfPath)) {
+            const pdfMTime: number = (await this.app.vault.adapter.stat(pdfPath))?.mtime ?? 0;
+
+            // if the pdf exists and has very similar modified date as xopp file just return the path and do nothing
+            if (pdfMTime - xoppFile.stat.mtime > 10) return pdfPath;
+        }
+        
+        // try to create directory
+        this.app.vault.adapter.mkdir(`${this.settings.importedPath}/preview`);
+
+        const vaultPath = this.getAbsoluteVaultPath();
+
+        exec(`xournalpp -p ${vaultPath}/${pdfPath} ${vaultPath}/${xoppFile.path}`);
+
+        return pdfPath;
+    }
+
     async loadSettings() {
         this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
     }
@@ -44,30 +70,26 @@ export default class XoppPlugin extends Plugin {
     async saveSettings() {
         await this.saveData(this.settings);
     }
-}
 
-class SettingTab extends PluginSettingTab {
-    plugin: XoppPlugin;
+    /**
+     * Workaround to get the vault path
+     * @returns string|null
+     */
+    getAbsoluteVaultPath(): string|null {
+        const matches = this.app.vault.adapter.getResourcePath('FAKE_FILE').match(/(?<=app:\/\/[^\ /\\]+\/)(.+)(?=\/FAKE_FILE\?\d+)/);
 
-    constructor(app: App, plugin: XoppPlugin) {
-        super(app, plugin);
-        this.plugin = plugin;
-    }
+        if (matches?.length) {
+            let path = matches[0];
 
-    display(): void {
-        const {containerEl} = this;
+            // if not on Windows, add a Slash before path
+            if (!path.match(/^[A-Z]:/)) {
+                path = '/' + path;
+            }
 
-        containerEl.empty();
-
-        new Setting(containerEl)
-            .setName('Setting #1')
-            .setDesc('It\'s a secret')
-            .addText(text => text
-                .setPlaceholder('Enter your secret')
-                .setValue(this.plugin.settings.mySetting)
-                .onChange(async (value) => {
-                    this.plugin.settings.mySetting = value;
-                    await this.plugin.saveSettings();
-                }));
+            return path;
+        }
+        else {
+            return null
+        }
     }
 }
