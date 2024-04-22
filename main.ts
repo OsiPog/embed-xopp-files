8e45f39572ea593d125319243acf24533c68fc27
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-04-22 12:27:57 +0200

    feat: intervally update embeds and make svg files

--- main.ts
+++ main.ts
@@ -1,69 +1,99 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, FileSystemAdapter, TFile, FileStats, Stat } from 'obsidian';
 import { exec } from 'child_process';
+import { MarkdownView, Plugin, TFile } from 'obsidian';
 
 interface XoppPluginSettings {
     importedPath: string;
 }
 
 const DEFAULT_SETTINGS: XoppPluginSettings = {
-    importedPath: 'xopp',
+    importedPath: 'Attachments',
 }
 
 export default class XoppPlugin extends Plugin {
     settings: XoppPluginSettings;
+    activeMDView: MarkdownView|null;
 
     async onload() {
         await this.loadSettings();
 
-        const divsEmbedFiles = this.app.workspace
-            .getActiveViewOfType(MarkdownView)?.contentEl
-            .querySelectorAll('div.file-embed');
-
-
-        const files: TFile[] = this.app.vault.getFiles();
-
-        if (!divsEmbedFiles) return;
-    
-        divsEmbedFiles.forEach( (e, i) => {
-            let filename: string = e.getAttribute('src') ?? '';
-            
-            // Skip embed files that are no XOPP file
-            if (!filename.match(/\.xopp$/)) return;
-
-            console.log(files.find(e => e.name == filename));
-        })
+        this.registerInterval(window.setInterval(() => {
+            this.activeMDView = this.app.workspace.getActiveViewOfType(MarkdownView);
+            this.updateEmbeds();
+        }, 1000))
     }
 
     onunload() {
 
     }
 
+    async updateEmbeds() {
+        if (!this.activeMDView) return;
+
+        const activeViewContent = this.activeMDView.contentEl
+
+        // get all embed xopp file divs from current document
+        const divsEmbedFiles = Array.from(activeViewContent?.querySelectorAll('div.file-embed') ?? [])
+        .filter(e => e.getAttribute('src')?.match(/\.xopp$/));
+
+        // all files in vault
+        const files: TFile[] = this.app.vault.getFiles();
+
+        divsEmbedFiles.forEach( async (div, i) => {
+            const filename: string|undefined = (div.getAttribute('src') ?? '')
+                // src might be a path
+                .replace(/\//, '/') // Windows path to unix path
+                .split('/')
+                .at(-1); // filename is last in list when path split by slash
+            
+            const xoppFile = files.find(e => e.name === filename);
+
+            if (!xoppFile) return;
+
+            const pngPaths = (await this.getPngFiles(xoppFile)).sort();
+
+            if (!div.classList.contains('has-xopp-images')) {
+                div.replaceChildren();
+                pngPaths.forEach((pngPath) => {
+                    const img = div.createEl('img');
+
+                    const file = this.app.vault.getFileByPath(pngPath);
+                    
+                    if (!file) return;
+
+                    img.setAttr('src', this.app.vault.getResourcePath(file));
+                })
+
+                div.classList.add('has-xopp-images');
+            }
+        })
+    }
+
     /**
      * Returns the generated preview pdf file for the given xopp file. If the pdf file is too old/doesn't exist generate one
-     * A pdf filename will have the form: path_to_xopp_xoppfilename.xopp.pdf
      * @param xoppFile
      */
-    async getPdfFile(xoppFile: TFile): Promise<TFile|null> {
-        const pdfPath = `${this.settings.importedPath}/preview/${xoppFile.path.replace(/\/|\\/, '_')}.pdf`;
+    async getPngFiles(xoppFile: TFile): Promise<string[]> {
+        const pngDir = `${this.settings.importedPath}/preview/${xoppFile.path.replaceAll(/\/|\\/g, '_').replace(/\.xopp$/g, "")}`;
 
-        if (await this.app.vault.adapter.exists(pdfPath)) {
-            const pdfMTime: number = (await this.app.vault.adapter.stat(pdfPath))?.mtime ?? 0;
+        if (await this.app.vault.adapter.exists(pngDir)) {
+            const pngMTime: number = 
+                (await this.app.vault.adapter.stat(`${pngDir}/exported`))?.mtime 
+                ?? (await this.app.vault.adapter.stat(`${pngDir}/exported-1`))?.mtime
+                ?? 0;
 
-            // if the pdf exists and has very similar modified date as xopp file just return the path and do nothing
-            if (pdfMTime - xoppFile.stat.mtime > 10) return this.app.vault.getFileByPath(pdfPath);
+            // if the exoport exists and has very similar modified date as xopp file just return the path and do nothing
+            if (pngMTime - xoppFile.stat.mtime > 10) 
+                return (await this.app.vault.adapter.list(pngDir)).files;
         }
         
         // try to create directory
-        this.app.vault.adapter.mkdir(`${this.settings.importedPath}/preview`);
+        this.app.vault.adapter.mkdir(pngDir);
 
         const vaultPath = this.getAbsoluteVaultPath();
 
-        return new Promise((resolve) => {
-            spawn('xournalpp', ['-p', `${vaultPath}/${pdfPath}`, `${vaultPath}/${xoppFile.path}`])
-                .on('close', () => {
-                    resolve(this.app.vault.getFileByPath(pdfPath));
-                });
-        })
+        exec(`xournalpp -i ${vaultPath}/${pngDir}/exported.svg ${vaultPath}/${xoppFile.path}`);
+
+        return (await this.app.vault.adapter.list(pngDir)).files;
     }
 
     async loadSettings() {
@@ -75,7 +105,7 @@ export default class XoppPlugin extends Plugin {
     }
 
     /**
-     * Workaround to get the vault path
+     * Workaround to get the full vault path
      * @returns string|null
      */
     getAbsoluteVaultPath(): string|null {
